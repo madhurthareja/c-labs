4b705be6fca69c523ad1330b430f21d02dec77bc
MadhurThareja01 <madhurthareja1105@gmail.com>
2024-05-08 13:19:52 +0530

    added w1

---
+++ W1/basic_prog1.c
@@ -0,0 +1,12 @@
+//This is my first C program
+
+#include <stdio.h>
+int main(){
+	int a = 100;
+	float f = 4.314;
+	char ch ='X';
+	printf("a = %d\n", a);
+	printf("f = %f\n", f);
+	printf("ch = %c\n", ch);
+	return 0;
+}
